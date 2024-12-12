165133dded7f34e4bfa4ee2dda6dd9923bd916a1
RohmaRifqiPamungkas <rohmarifqi31@gmail.com>
2024-12-12 17:37:15 +0700

    Commit Authentication

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username', 50)->unique();
+            $table->string('full_name', 100);
+            $table->string('phone', 20)->nullable();
+            $table->date('birthday_date')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->integer('is_active')->default(1);
             $table->timestamps();
         });
 
