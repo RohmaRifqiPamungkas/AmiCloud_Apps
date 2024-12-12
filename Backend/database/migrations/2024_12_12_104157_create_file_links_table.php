a285502cf76a2887e1d6e6a311c4d2c23645d286
RohmaRifqiPamungkas <rohmarifqi31@gmail.com>
2024-12-12 18:12:09 +0700

    Commit migrate databases

---
+++ Backend/database/migrations/2024_12_12_104157_create_file_links_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('file_links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('file_path', 255);
+            $table->text('original_url');
+            $table->text('parsed_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('file_links');
+    }
+};
