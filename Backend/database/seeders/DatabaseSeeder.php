8ff4440d0cd02fb839405dafd7fb476540ff7e6a
RohmaRifqiPamungkas <rohmarifqi31@gmail.com>
2024-12-16 09:51:18 +0700

    Commit Auth and Reset Password

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -18,8 +18,6 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            // 'username' => 'testuser',
-            // 'full_name' => 'Test User Full Name',
         ]);
     }
 }
